129678732d4a877d7b139b346df2006032710479
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-01-10 20:50:08 +0600

    added frist two navigation

---
+++ App.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CategoryScreen from "./screens/CategoryScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+//create stack
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategory" component={CategoryScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
