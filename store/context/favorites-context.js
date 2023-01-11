5f2c5369cb2c12ee0beb4f5f8c987012abaab2f0
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-01-11 20:20:51 +0600

    added change favorite status with useContext

---
+++ store/context/favorites-context.js
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+function FavoritesContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  //add favorite meals id
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+  //remove from favids
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export default FavoritesContextProvider;
